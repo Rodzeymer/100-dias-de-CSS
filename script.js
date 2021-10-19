cd637b1b51d3c29b100a8c9d060cec090b54b6a0
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2021-10-19 08:09:41 -0300

    Será que vai?

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 var textField = document.getElementById('txtUrl');
+console.log(textField);
 
-textField.addEventListener('keyup', function() {
-    document.querySelector('iframe').setAttribute('src', textField.value);
-})
+function irpUrl(textField){
+    getElementById("urlDestino").innerHTML("src=('https://www.youtube.com/embed/') + textField");
+    console.log(textField);
+}
