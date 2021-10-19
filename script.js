41af51c9bf64b85fce367355d9b74ee7f223d6b8
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2021-10-19 09:10:25 -0300

    36

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
-
-
-function irpUrl(){   
     var textField = document.getElementById('txtUrl');
     console.log(textField);
+
+
+function irpUrl(){ 
     var destino = "https://www.youtube.com/embed/" + textField;
     console.log(destino);
     console.log(destino.value);
