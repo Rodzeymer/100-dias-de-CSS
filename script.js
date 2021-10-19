e9c034e2b6c3cace61762a2ccf95c095b7d420da
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2021-10-19 08:38:32 -0300

    FOI?

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 var textField = document.getElementById('txtUrl');
-var irPara = getElementById("urlDestino");
-console.log(textField);
-console.log(txtUrl);
 
-console.log(irPara);
-console.log(urlDestino);
 
 function irpUrl(){   
-    document.getElementById("urlDestino").src = "https://www.youtube.com/embed/" + textField;
-    console.log(textField);
+    var vamos = document.getElementById("urlDestino")
+    .src = "https://www.youtube.com/embed/" + textField;
+    console.log(vamos.src);
 }
