ca511adb2c2064ad7f350da5df3f485cad95bd2f
Rodzeymer <60178572+Rodzeymer@users.noreply.github.com>
2021-10-19 08:27:12 -0300

    25

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var textField = document.getElementById('txtUrl');
 console.log(textField);
 
 function irpUrl(){
-    getElementById("urlDestino").innerHTML("src=('https://www.youtube.com/embed/') + textField");
+   var irPara = getElementById("urlDestino")
+   .innerHTML("src=('https://www.youtube.com/embed/') + textField");
     console.log(textField);
 }
